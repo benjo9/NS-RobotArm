ce311f656c436f876a803e6eb1fafb0765e41e6c
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-04-24 12:18:55 +0200

    Trajectory planning updated

---
+++ robot_ws/build/industrial_robot_simulator/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/noa/Desktop/Robot_Repo/robot_ws/src/industrial_robot_simulator
+# Build directory: /home/noa/Desktop/Robot_Repo/robot_ws/build/industrial_robot_simulator
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
