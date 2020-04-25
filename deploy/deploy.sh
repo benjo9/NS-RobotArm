3644c165f4d76914c89d8834e7fec7780d51f210
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-04-25 23:04:30 +0200

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,7 @@
 
 if [ -d ~/NS-RobotArm/deploy/install ]; then rm -Rf ~/NS-RobotArm/deploy/install; fi
 cp -r ~/NS-RobotArm/robot_ws/install/ ~/NS-RobotArm/deploy/
+cp -r ~/NS-RobotArm/arduino/ ~/NS-RobotArm/deploy/
 git add .
 git commit -m "deploy"
 git push -u origin master
