54866c15250f6f1a8ed70448b27c6247b81f3ad6
Noothless <42466108+Noothless@users.noreply.github.com>
2020-04-25 14:48:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ cd ~/NS-RobotArm/robot_ws/src/
 git clone https://github.com/jmeyer1292/fake_ar_publisher.git
 cd ..
 catkin init
+catkin config --extend /opt/ros/melodic
 catkin build
 source devel/setup.bash
