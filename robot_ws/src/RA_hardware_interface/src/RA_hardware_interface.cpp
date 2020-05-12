d920b78866ad068497938089d8d7e85f451ef274
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-05-13 00:50:37 +0200

    CMake

--- robot_ws/src/RA_hardware_interface/src/RA_hardware_interface.cpp
+++ robot_ws/src/RA_hardware_interface/src/RA_hardware_interface.cpp
@@ -5,8 +5,8 @@
 #include <joint_limits_interface/joint_limits_urdf.h>
 #include <joint_limits_interface/joint_limits_rosparam.h>
 #include "ros/ros.h"
-#include "hardware_interface/driver.h"
-#include "hardware_interface/feedback.h"
+#include "RA_hardware_interface/driver.h"
+#include "RA_hardware_interface/feedback.h"
 
 
 using namespace hardware_interface;
