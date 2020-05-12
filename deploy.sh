229b0ac83279914cce85979f16b6d65e3cb19294
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-05-12 16:29:18 +0200

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+git pull origin master
+cd ~/NS-RobotArm/robot_ws/
+catkin build
+cd ~/NS-RobotArm/
 if [ -d ~/NS-RobotArm/deploy/install ]; then rm -Rf ~/NS-RobotArm/deploy/install; fi
 if [ -d ~/NS-RobotArm/deploy/arduino ]; then rm -Rf ~/NS-RobotArm/deploy/arduino; fi
 cp -r ~/NS-RobotArm/robot_ws/install/ ~/NS-RobotArm/deploy/
