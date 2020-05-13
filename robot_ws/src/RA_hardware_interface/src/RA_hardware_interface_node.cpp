08f25261c248e8ccd0d79f8c84aa75aa66459e36
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-05-13 14:51:54 +0200

    HI

--- robot_ws/src/RA_hardware_interface/src/RA_hardware_interface_node.cpp
+++ robot_ws/src/RA_hardware_interface/src/RA_hardware_interface_node.cpp
@@ -4,7 +4,6 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "RA_hardware_interface");
 
     ros::NodeHandle nh;
-    nh.setCallbackQueue(&ros_queue);
     RA_hardware_interface::RAHardwareInterface rhi(nh);
 
     ros::MultiThreadedSpinner spinner(0);
