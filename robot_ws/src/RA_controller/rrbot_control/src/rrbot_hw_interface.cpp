955d3a241007ff75917ee7916be513d9a295d089
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-05-18 18:42:20 +0200

    std

--- robot_ws/src/RA_controller/rrbot_control/src/rrbot_hw_interface.cpp
+++ robot_ws/src/RA_controller/rrbot_control/src/rrbot_hw_interface.cpp
@@ -60,7 +60,7 @@ void RRBotHWInterface::read(ros::Duration &elapsed_time)
     feedback.request.axis = i + 1;
     float p = axis_position.call(feedback);
     joint_position_[i] = p;
-    string s = std::to_string(p);
+    std::string s = std::to_string(p);
     ROS_INFO(s);
     //ROS_INFO("read:2");
   }
