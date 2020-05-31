45b44331e08cc416537eb6e5fd0a3238a94ad0b4
Noothless <42466108+Noothless@users.noreply.github.com>
2020-05-31 14:59:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ sudo apt-get install libgflags-dev
 sudo apt-get install ros-$ROS_DISTRO-rosparam-shortcuts
 sudo apt-get install ros-melodic-ros-control ros-melodic-ros-controllers
 sudo apt install python3 python3-pip
+sudo apt install ros-kinetic-rosparam-shortcuts
 pip3 install odrive
 cd ~/NS-RobotArm/robot_ws/src/
 git clone https://github.com/jmeyer1292/fake_ar_publisher.git
