18bb94f8337e598e6f4bf9c467d02033f255a50c
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-06-03 21:17:25 +0200

    driver

--- robot_ws/src/RA_controller/rrbot_control/src/rrbot_hw_interface.cpp
+++ robot_ws/src/RA_controller/rrbot_control/src/rrbot_hw_interface.cpp
@@ -108,11 +108,11 @@ void RRBotHWInterface::write(ros::Duration &elapsed_time)
       drive_pub1.publish(msg);
     } else if(i == 1)
     {
-      msg.data = round(joint_position_command_[i]*1024000/pi);
+      msg.data = round(joint_position_command_[i]*(-1024000)/pi);
       drive_pub2.publish(msg);
     } else if(i == 2)
     {
-      msg.data = round(joint_position_command_[i]*204800/pi);
+      msg.data = round(joint_position_command_[i]*(-204800)/pi);
       drive_pub3.publish(msg);
     } else if(i == 3)
     {
