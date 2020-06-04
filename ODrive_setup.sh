0809922918e4a8e221c45e788ece2b8f21a8911e
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-06-04 21:03:43 +0200

    bash

--- ODrive_setup.sh
+++ ODrive_setup.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
-echo "Press any key to continue"
+cd robot_ws/
+source install/setup.bash
+rosservice call /axis_requested_state 3 3
+echo "Axis 3 callibration: Press any key to continue"
 read -n 1
+rosservice call /axis_requested_state 3 11
+echo "Axis 3 homing: Press any key to continue"
+read -n 1
+rosservice call /axis_requested_state 3 8
+sleep 2
+rosservice call /axis_requested_state 1 3
+sleep 0.5
+rosservice call /axis_requested_state 2 3
+sleep 0.5
+rosservice call /axis_requested_state 4 3
+sleep 0.5
+rosservice call /axis_requested_state 5 3
+sleep 0.5
+rosservice call /axis_requested_state 6 3
+echo "All axis callibration: Press any key to continue"
+read -n 1
+rosservice call /axis_requested_state 1 8
+sleep 0.5
+rosservice call /axis_requested_state 2 8
+sleep 0.5
+rosservice call /axis_requested_state 4 8
+sleep 0.5
+rosservice call /axis_requested_state 5 8
+sleep 0.5
+rosservice call /axis_requested_state 6 8
